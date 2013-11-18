de5695399af2f1dd74dc097f255fda8fd9a9867a
Gabriel Massana <gabriel.massana@irisvalet.com>
2013-11-18 09:51:19 +0000

    Initial Commit

---
+++ FB_Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FB_Twitter
+//
+//  Created by Gabriel Massana on 11/18/13.
+//  Copyright (c) 2013 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FB_Twitter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FB_Twitter
+//
+//  Created by Gabriel Massana on 11/18/13.
+//  Copyright (c) 2013 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
